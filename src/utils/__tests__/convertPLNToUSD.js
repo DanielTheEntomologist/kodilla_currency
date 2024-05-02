ba2a8de01a7234b5c64b6751b545fa9dfcc91089
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-05-02 18:43:40 +0200

    Test convertPLNToUSD

--- src/utils/__tests__/convertPLNToUSD.js
+++ src/utils/__tests__/convertPLNToUSD.js
@@ -23,5 +23,31 @@ describe("ConvertPLNtoUSD", () => {
 // Sprawdzić dla kilku wartości, czy dla dobrych inputów zwróci dobry wynik w dobrym formacie.
 
 // Sprawdzić, czy dla argumentów, które nie są tekstem ani liczbą, funkcja wyrzuci błąd.
+describe("ConvertPLNtoUSD", () => {
+  it("for nonstring nonnumber arguments should return 'Error'", () => {
+    expect(convertPLNToUSD(Object)).toBe("Error");
+    expect(convertPLNToUSD([])).toBe("Error");
+    expect(convertPLNToUSD({})).toBe("Error");
+    expect(convertPLNToUSD(null)).toBe("Error");
+    expect(convertPLNToUSD(true)).toBe("Error");
+    expect(convertPLNToUSD(function () {})).toBe("Error");
+  });
+});
+
 // Sprawdzić, czy dla argumentu liczbowego, ale mniejszego niż zero, funkcja zwróci “$0.00“.
+describe("ConvertPLNtoUSD", () => {
+  it("should return  “$0.00“ for number below 0", () => {
+    expect(convertPLNToUSD(-1)).toBe("$0.00");
+    expect(convertPLNToUSD(-0.0123)).toBe("$0.00");
+    expect(convertPLNToUSD(-12)).toBe("$0.00");
+    expect(convertPLNToUSD(-22)).toBe("$0.00");
+  });
+});
+
 // Sprawdzić, czy brak argumentu spowoduje zwrócenie wartości NaN.
+describe("ConvertPLNtoUSD", () => {
+  it("should return NaN for no argument or undefined arg", () => {
+    expect(convertPLNToUSD()).toBeNaN();
+    expect(convertPLNToUSD(undefined)).toBeNaN();
+  });
+});
