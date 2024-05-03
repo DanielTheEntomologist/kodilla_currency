af9bd694ff8a61a3b27fbaf568b09211822374d0
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-05-03 21:55:24 +0200

    Test ResultBox Component

--- src/components/ResultBox/ResultBox.js
+++ src/components/ResultBox/ResultBox.js
@@ -1,31 +1,40 @@
-import PropTypes from 'prop-types';
-import { convertUSDToPLN } from './../../utils/convertUSDToPLN';
-import { convertPLNToUSD } from './../../utils/convertPLNToUSD';
-import { formatAmountInCurrency } from './../../utils/formatAmountInCurrency';
-import { useMemo } from 'react';
-import styles from './ResultBox.module.scss';
+import PropTypes from "prop-types";
+import { convertUSDToPLN } from "./../../utils/convertUSDToPLN";
+import { convertPLNToUSD } from "./../../utils/convertPLNToUSD";
+import { formatAmountInCurrency } from "./../../utils/formatAmountInCurrency";
+import { useMemo } from "react";
+import styles from "./ResultBox.module.scss";
 
 const ResultBox = ({ from, to, amount }) => {
-
   const convertedAmount = useMemo(() => {
-    if(from === 'USD' && to === 'PLN') return convertUSDToPLN(amount);
-    if(from === 'PLN' && to === 'USD') return convertPLNToUSD(amount);
+    if (from === "USD" && to === "PLN") return convertUSDToPLN(amount);
+    if (from === "PLN" && to === "USD") return convertPLNToUSD(amount);
     return formatAmountInCurrency(amount, from);
   }, [from, to, amount]);
 
-  const formattedAmount = useMemo(() => formatAmountInCurrency(amount, from), [amount, from]);
-
-  return (
-    <div className={styles.result}>
-      {formattedAmount} = {convertedAmount}
-    </div>
+  const formattedAmount = useMemo(
+    () => formatAmountInCurrency(amount, from),
+    [amount, from]
   );
+  if (amount < 0) {
+    return (
+      <div className={styles.result} data-testid="result-box">
+        {"Wrong amount!"}
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.result} data-testid="result-box">
+        {formattedAmount} = {convertedAmount}
+      </div>
+    );
+  }
 };
 
 ResultBox.propTypes = {
   from: PropTypes.string.isRequired,
   to: PropTypes.string.isRequired,
   amount: PropTypes.number.isRequired,
-}
+};
 
 export default ResultBox;
